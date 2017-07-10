d5d24a263a6db7d22e06623db78c0d031d822a98
Mohanad Elhag <moh.7afiz@gmail.com>
2017-07-11 02:43:24 +0300

    finished project

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { Http, Headers } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
+
+import { MoviesService } from './services/movies.service';
+import { AboutComponent } from './components/about/about.component';
+
+import { appRoutingProvider } from './app.routes';
+import { MovieComponent } from './components/movie/movie.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MoviesComponent,
+    AboutComponent,
+    MovieComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    JsonpModule,
+    FormsModule,
+    RouterModule,
+    appRoutingProvider
+  ],
+  providers: [MoviesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
